ed52bd974777b235a161f556c875b9c2d56a8f17
trung.hoang <hoangtrung.huster@gmail.com>
2020-02-01 22:27:50 +0700

    Updates

---
+++ precache-manifest.716a97303deb569187884368a1c5066c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf8ee55dc42f79091a64e4fff596a3ad",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f7988719a669b5b6dbaa",
+    "url": "/tic-tac-toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "69ce7d1b22d975f60ec3",
+    "url": "/tic-tac-toe/static/js/2.21bec9a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.21bec9a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7988719a669b5b6dbaa",
+    "url": "/tic-tac-toe/static/js/main.6ea8e1fe.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  }
+]);
